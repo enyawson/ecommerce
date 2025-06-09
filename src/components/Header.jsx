017d42eb63b347f82b7d9e8afea15ffc2dd198fc
enyawson <enyawson33@gmail.com>
2025-06-09 23:37:32 +0000

    add menu list to mobile menu icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,10 @@ import {
   IconButton,
   Stack,
   useColorModeValue,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from '@chakra-ui/react';
 import { HiOutlineShoppingCart, HiOutlineMenu } from 'react-icons/hi';
 import useCartStore from '../store/cartStore';
@@ -38,7 +42,7 @@ const Header = () => {
             spacing={8}
             display={{ base: 'none', md: 'flex' }}
           >
-            <Link as={RouterLink} to="/" color="white" _hover={{ color: 'orange.400' }} fontWeight="bold" fontSize="1.2rem" >Home</Link>
+            <Link as={RouterLink} to="/" color="white" _hover={{ color: 'orange.400' }} fontWeight="bold" fontSize="1.2rem">Home</Link>
             <Link as={RouterLink} to="/category/headphones" color="white" _hover={{ color: 'orange.400' }} fontWeight="bold" fontSize="1.2rem">Headphones</Link>
             <Link as={RouterLink} to="/category/speakers" color="white" _hover={{ color: 'orange.400' }} fontWeight="bold" fontSize="1.2rem">Speakers</Link>
             <Link as={RouterLink} to="/category/earphones" color="white" _hover={{ color: 'orange.400' }} fontWeight="bold" fontSize="1.2rem">Earphones</Link>
@@ -75,15 +79,57 @@ const Header = () => {
                 )}
               </Box>
             </Link>
-            <IconButton
-              display={{ base: 'flex', md: 'none' }}
-              ml={4}
-              icon={<HiOutlineMenu size={24} />}
-              variant="ghost"
-              color="white"
-              aria-label="Open menu"
-              _hover={{ bg: 'whiteAlpha.200' }}
-            />
+            <Box display={{ base: 'block', md: 'none' }}>
+              <Menu>
+                <MenuButton
+                  as={IconButton}
+                  ml={4}
+                  icon={<HiOutlineMenu size={24} />}
+                  variant="ghost"
+                  color="white"
+                  aria-label="Open menu"
+                  _hover={{ bg: 'whiteAlpha.200' }}
+                />
+                <MenuList bg="black" borderColor="gray.600">
+                  <MenuItem 
+                    as={RouterLink} 
+                    to="/" 
+                    _hover={{ bg: 'gray.600', color: 'orange.400' }}
+                    bg="black" 
+                    color="white"
+                  >
+                    Home
+                  </MenuItem>
+                  <MenuItem 
+                    as={RouterLink} 
+                    to="/category/headphones"
+                    _hover={{ bg: 'gray.600', color: 'orange.400' }}
+                    bg="black"
+                    color="white" 
+                  >
+                    Headphones
+                  </MenuItem>
+                  <MenuItem 
+                    as={RouterLink} 
+                    to="/category/speakers"
+                    _hover={{ bg: 'gray.600', color: 'orange.400' }}
+                    bg="black"
+                    color="white"
+                  >
+                    Speakers
+                  </MenuItem>
+                  <MenuItem 
+                    as={RouterLink} 
+                    to="/category/earphones"
+                    _hover={{ bg: 'gray.600', color: 'orange.400' }}
+                    bg="black"
+                    color="white"
+                  >
+                    Earphones
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Box>
           </Flex>
         </Flex>
       </Container>
